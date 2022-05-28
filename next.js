3c880b09d43d4901a28ccb19fe6d880b5d972a9b
Thomas Cazade <cazade.thomas@gmail.com>
2022-05-28 18:36:25 +0200

    Update next ESLint plugin import

--- next.js
+++ next.js
@@ -2,5 +2,5 @@
 
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: [require.resolve('./index'), 'next'],
+  extends: [require.resolve('./index'), 'plugin:@next/next/recommended'],
 };

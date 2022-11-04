af28c8bf478766d77b722dc29c61187e3a38a3f6
Thomas Cazade <cazade.thomas@gmail.com>
2022-11-04 16:38:57 +0100

    Rework Next.js ESLint config

--- next.js
+++ next.js
@@ -2,5 +2,20 @@
 
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: [require.resolve('./index'), 'next/core-web-vitals'],
+  extends: [
+    'eslint:recommended',
+
+    'plugin:eslint-comments/recommended',
+    'plugin:unicorn/recommended',
+    'plugin:promise/recommended',
+
+    'airbnb',
+    'airbnb-typescript',
+
+    // Includes jsx-a11y, react, react-hooks.
+    // A lot of custom rules for Next.js as well.
+    'next/core-web-vitals',
+
+    'plugin:prettier/recommended',
+  ],
 };
